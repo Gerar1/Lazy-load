22731a3873306c750a79052713f626797db67d87
Gerar <gerbas.cc@gmail.com>
2022-12-03 15:49:00 -0300

    lazy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, {Suspense, lazy} from 'react';
 import './App.css';
+const Componente = lazy(()=>import('./components/Componente'));
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Suspense fallback={<h2>Espereme amiguito</h2>}>
+     <Componente/>
+    </Suspense>
     </div>
   );
 }
